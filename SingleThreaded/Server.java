a8e4d9c44295d7c087f6b59a6dc9d24eafc14a9f
Rohit Priyadarshi <rohitp2203@gmail.com>
2025-03-03 23:09:07 +0530

    dev branch added

--- SingleThreaded/Server.java
+++ SingleThreaded/Server.java
@@ -1,25 +1,27 @@
+package SingleThreaded;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server{
+public class Server {
 
-    public void run() throws IOException{
+    public void run() throws IOException {
         int port = 8010;
         ServerSocket socket = new ServerSocket(port);
         socket.setSoTimeout(10000);
-        while(true){
-            try{
-            System.out.println("Server is listening on port " + port);
-            Socket acceptedConnection = socket.accept();
-            }catch(IOException ex){
+        while (true) {
+            try {
+                System.out.println("Server is listening on port " + port);
+                Socket acceptedConnection = socket.accept();
+            } catch (IOException ex) {
                 ex.printStackTrace();
             }
         }
 
     }
 
-    public static void main(String args){
+    public static void main(String args) {
 
     }
 }
