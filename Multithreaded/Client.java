dd9cb077b07e803459d53e82879d3fd7a586e9d8
Rohit Priyadarshi <rohitp2203@gmail.com>
2025-03-08 23:27:32 +0530

    Multithreaded Server

---
+++ Multithreaded/Client.java
@@ -0,0 +1,26 @@
+
+
+public class Client {
+
+    public Runnable getRunnable() {
+        return new Runnable(){
+            @Override
+            public void run(){
+                int port = 8010;
+            }
+        };
+    }
+    
+    public static void main(String[] args) {
+        Client client = new Client();
+        for(int i=0;i<100;i++){
+            try {
+                Thread thread = new Thread(client.getRunnable());
+                thread.start();
+            } catch (Exception e) {
+                return;
+            }
+        }
+    }
+    
+}
---
+++ Multithreaded/Server.java
@@ -0,0 +1,38 @@
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.function.Consumer;
+import java.io.IOException;
+
+public class Server {
+
+    public Consumer <Socket> getConsumer(){
+        return(clientSocket)->{
+            try {
+                PrintWriter toCllient = new PrintWriter(clientSocket.getOutputStream());
+                toCllient.println("Hello from the server");
+                toCllient.close();
+                clientSocket.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = 8010;
+        Server server = new Server();
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            serverSocket.setSoTimeout(10000);
+            System.out.println("Server is listening on port " + port);
+            while (true) {
+                Socket acceptedSocket = serverSocket.accept();
+                Thread thread = new Thread(() -> server.getConsumer().accept(acceptedSocket));
+                thread.start();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
